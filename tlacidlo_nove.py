ed05283b6eb52f2a90b1798fb2843ba00859bdc6
AdvencedmanSK <patrik.sramek0@gmail.com>
2020-08-09 23:14:45 +0200

    pridanie tlacidla faktorial ktore spusti def faktorial

--- tlacidlo_nove.py
+++ tlacidlo_nove.py
@@ -5,6 +5,13 @@ okno = tk.Tk()
 odist = tk.Button(text="odísť",fg="red", command=quit)
 odist.pack(side=tk.LEFT)
 
+    def faktorial():
+        print(bruh)
+
+faktorial_tlacidlo = tk.Button(text="vypočítať faktoriál", fg="green", command=quit)
+faktorial_tlacidlo.pack(side=tk.LEFT)
+
+
 
 
 okno.mainloop()
