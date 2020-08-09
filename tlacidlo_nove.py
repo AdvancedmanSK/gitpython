e39f63aaab60d4fee78f3cd8e66bd8f325f9677f
AdvencedmanSK <patrik.sramek0@gmail.com>
2020-08-09 23:10:11 +0200

    pridanie cervenej farby tlacidlu odist

--- tlacidlo_nove.py
+++ tlacidlo_nove.py
@@ -2,7 +2,9 @@ import tkinter as tk
 
 okno = tk.Tk()
 
-odist = tk.Button(text="odísť", command=quit)
+odist = tk.Button(text="odísť",fg="red", command=quit)
 odist.pack(side=tk.LEFT)
+
+
 
 okno.mainloop()
