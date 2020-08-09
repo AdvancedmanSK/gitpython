3bd1996e98c81321ff350defe43ee0f510675c32
AdvencedmanSK <patrik.sramek0@gmail.com>
2020-08-09 23:33:32 +0200

    pridanie uvitacieho labelu

--- tlacidlo_nove.py
+++ tlacidlo_nove.py
@@ -11,6 +11,11 @@ def faktorial():
     faktorial_okno.title("Faktorial")
     faktorial_okno.geometry("200x200")
 
+    vytaj = tk.Label(faktorial_okno, text="Vypočítaj si faktoriál",font = "Helvetica 16 bold italic", fg="Blue")
+    vytaj.place(relx=0.2, rely=0.0)
+
+
+
 faktorial_tlacidlo = tk.Button(okno, text="vypočítať faktoriál", fg="green", command=faktorial)
 faktorial_tlacidlo.pack(side=tk.LEFT)
 
