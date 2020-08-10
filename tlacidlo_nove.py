e72dbba945db8b396affc502abe52b8ca6793e93
AdvencedmanSK <patrik.sramek0@gmail.com>
2020-08-10 19:18:10 +0200

    pridanie funkcie faktorial

--- tlacidlo_nove.py
+++ tlacidlo_nove.py
@@ -19,9 +19,17 @@ def faktorial():
     zadanie_cisel = tk.Label(faktorial_okno, text="Zadaj číslo:", font="Canvas 9", fg="green")
     zadanie_cisel.place(x=20, y=30)
 
-
-    vstup = tk.Entry(faktorial_okno)
+    global vstup
+    vstup = int(tk.Entry(faktorial_okno))
     vstup.place(x=100, y=30)
+
+    def vypocet_faktorialu():
+        n = vstup
+        fact = 1
+    
+        for i in range(1,n+1): 
+            fact = fact * i 
+        print(fact)
 
 
 
