41388b11d949dd808d8190c68a76d5f9943f7c57
AdvencedmanSK <patrik.sramek0@gmail.com>
2020-08-10 19:29:30 +0200

    skušanie int na entry

--- tlacidlo_nove.py
+++ tlacidlo_nove.py
@@ -19,17 +19,19 @@ def faktorial():
     zadanie_cisel = tk.Label(faktorial_okno, text="Zadaj číslo:", font="Canvas 9", fg="green")
     zadanie_cisel.place(x=20, y=30)
 
-    global vstup
-    vstup = int(tk.Entry(faktorial_okno))
+    global n
+    vstup = tk.Entry(faktorial_okno)
     vstup.place(x=100, y=30)
+    n=int(vstup)
 
     def vypocet_faktorialu():
-        n = vstup
         fact = 1
     
         for i in range(1,n+1): 
             fact = fact * i 
         print(fact)
+
+
 
 
 
