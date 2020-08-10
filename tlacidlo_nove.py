5bb06b5be1996c28d5a117c2ddef7a8002a37f48
AdvencedmanSK <patrik.sramek0@gmail.com>
2020-08-10 19:03:55 +0200

    opravenie tlacidiel na place

--- tlacidlo_nove.py
+++ tlacidlo_nove.py
@@ -4,7 +4,7 @@ okno = tk.Tk()
 okno.title("Prvý program")
 
 odist = tk.Button(okno, text="odísť",fg="red", command=quit)
-odist.pack(side=tk.LEFT)
+odist.place(relx=0.0, rely=0.0)
 
 
 #nove okno pre faktorial
@@ -15,13 +15,17 @@ def faktorial():
 
     vytaj = tk.Label(faktorial_okno, text="Vypočítaj si faktoriál",font = "Helvetica 14 bold italic", fg="Blue")
     vytaj.place(relx=0.2, rely=0.0)
+
+    zadanie_cisel = tk.Label(faktorial_okno)
+
+
     vstup = tk.Entry(faktorial_okno)
     vstup.place(relx=0.32, rely=0.15)
 
 
 
 faktorial_tlacidlo = tk.Button(okno, text="vypočítať faktoriál", fg="green", command=faktorial)
-faktorial_tlacidlo.pack(side=tk.LEFT)
+faktorial_tlacidlo.place(relx=0.3, rely=0.0)
 
 
 
