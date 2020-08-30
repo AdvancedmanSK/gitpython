75fc2dad974693a069a04858fc0c072570140a41
AdvencedmanSK <patrik.sramek0@gmail.com>
2020-08-30 19:43:03 +0200

    vytvorenie napiču kódu

---
+++ kokot.py
@@ -0,0 +1,12 @@
+vstup = input(str("zadaj ideológiu: "))
+
+if vstup == "lsns":
+    print("fajinovy chlopak")
+elif vstup == "libertarianizmus":
+    print("Všetky babenky sú vlhké")
+elif vstup == "neoliberalizmus":
+    print("Pracuješ v bani")
+elif vstup == "komunizmuzs":
+    print("McNukes idú na teba")
+else:
+    print("Si kokot")
