0b8b957b8f73c60142a3c20e4295229da2bb5590
AdvencedmanSK <patrik.sramek0@gmail.com>
2020-08-30 19:27:17 +0200

    Spravenie verzie 1.0

--- citanie_txt.py
+++ citanie_txt.py
@@ -4,5 +4,18 @@ for line in crimefile.readlines():
     for i in line.split(","):
         row.append(i)
 
+def meno():
+    for i in range(0, len(row), 3):
+        print(row[i])
+meno()
 
-print(row)
+def povolanie():
+    for i in range(1, len(row), 3):
+        print(row[i])
+povolanie()
+
+def age():
+    for i in range(2, len(row), 3):
+        print(row[i])
+age()
+    
