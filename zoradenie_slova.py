0130316d1fc3cc5bd9e061abde65b3638dcfb51b
AdvencedmanSK <patrik.sramek0@gmail.com>
2020-09-04 15:10:09 +0200

    pridanie funkcie zoradenia

--- zoradenie_slova.py
+++ zoradenie_slova.py
@@ -1,8 +1,9 @@
 pismeno = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","r","s","t","u","v","x","y","z"]
-tocenie = int(0)
 slovo = input(str("zadaj solov: "))
 cislo_pismen = []
+cislo_pismen_upravene = []
 def premena_index():
+    tocenie = int(0)
     for i in (slovo): #rozloženie slova po pismenach
         for j in range(24): #porovnavanie pismena(slovo) s listom pismeno a udavania indexu pismena
             if i==pismeno[tocenie]:
@@ -11,6 +12,13 @@ def premena_index():
                 break
             tocenie = tocenie + 1
 premena_index()
+
+def zoradenie(zaklad, postupne):
+    for i in range(len(zaklad)):
+        postupne.append(max(zaklad))
+        zaklad.remove(max(zaklad))
+    print(postupne)
+
 print(cislo_pismen)
 
 
