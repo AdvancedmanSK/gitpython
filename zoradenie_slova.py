88a1b64cf75a13bee27155450438dd7e7bbbd14b
AdvencedmanSK <patrik.sramek0@gmail.com>
2020-09-04 15:52:55 +0200

    Finalny program 1.0

--- zoradenie_slova.py
+++ zoradenie_slova.py
@@ -1,11 +1,16 @@
-pismeno = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","r","s","t","u","v","x","y","z"]
+from typing import final
+
+pismeno = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," "]
 slovo = input(str("zadaj solov: "))
 cislo_pismen = []
 cislo_pismen_upravene = []
+cislo_pismen_upravene_pismeno = []
+
+
 def premena_index():
     tocenie = int(0)
     for i in (slovo): #rozloženie slova po pismenach
-        for j in range(24): #porovnavanie pismena(slovo) s listom pismeno a udavania indexu pismena
+        for j in range(27): #porovnavanie pismena(slovo) s listom pismeno a udavania indexu pismena
             if i==pismeno[tocenie]:
                 cislo_pismen.insert(0, tocenie)
                 tocenie = int(0)
@@ -14,12 +19,26 @@ def premena_index():
 premena_index()
 
 def zoradenie(zaklad, postupne):
-    for i in range(len(zaklad)):
-        postupne.append(max(zaklad))
-        zaklad.remove(max(zaklad))
-    print(postupne)
+    for i in range(len(zaklad)):    #pridanie stareho kodu na zoradenie čísel
+        postupne.append(min(zaklad))
+        zaklad.remove(min(zaklad))
 
-print(cislo_pismen)
+zoradenie(cislo_pismen, cislo_pismen_upravene)
+
+def finnal():
+    global final
+    final = str()
+    n = int(0)
+    for i in range (len(cislo_pismen_upravene)):    #premena čísla na písmená
+        cislo_pismen_upravene_pismeno.append(pismeno[cislo_pismen_upravene[n]])
+        n = n + 1
+    final = ("").join(cislo_pismen_upravene_pismeno)
+        
+    
+finnal()        
+        
+
+print(final)
 
 
 
