e75353e0de4a3855706291f68797977996f1cd0a
AdvencedmanSK <patrik.sramek0@gmail.com>
2020-09-04 15:04:57 +0200

    pridanie do funkcie

--- zoradenie_slova.py
+++ zoradenie_slova.py
@@ -2,13 +2,15 @@ pismeno = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","r","
 tocenie = int(0)
 slovo = input(str("zadaj solov: "))
 cislo_pismen = []
-for i in (slovo): #rozloženie slova po pismenach
-    for j in range(24): #porovnavanie pismena(slovo) s listom pismeno a udavania indexu pismena
-        if i==pismeno[tocenie]:
-            cislo_pismen.insert(0, tocenie)
-            tocenie = int(0)
-            break
-        tocenie = tocenie + 1
+def premena_index():
+    for i in (slovo): #rozloženie slova po pismenach
+        for j in range(24): #porovnavanie pismena(slovo) s listom pismeno a udavania indexu pismena
+            if i==pismeno[tocenie]:
+                cislo_pismen.insert(0, tocenie)
+                tocenie = int(0)
+                break
+            tocenie = tocenie + 1
+premena_index()
 print(cislo_pismen)
 
 
