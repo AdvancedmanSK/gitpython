22d9f787a58be6f04fcf88028c06321f92ca8d50
AdvencedmanSK <patrik.sramek0@gmail.com>
2020-09-10 14:28:20 +0200

    Fajinový prototyp aj s počítadlom pravdepodobnosti

--- narodeniny.py
+++ narodeniny.py
@@ -1,21 +1,32 @@
 import random
-
-dni = [1,1,356,356]
+global n,spravne
 n = int(input("zadaj číslo :"))
-def zaklad():
-    for i in range(n):
-        dni.append(random.randint(1,357))
-        dni.sort()
-    
-def porovnanie():
-    for i in range(0,n+3):
-        if dni[i] == dni[i+1]:
-            print("Toto číslo je rovnaké: "+ str(dni[i]))
+spravne = int(0)
+for i in range (100):
+    dni = []
+    def zaklad():
+        for i in range(n):
+            dni.append(random.randint(1,357))
+            dni.sort()
+        
+    def porovnanie():
+        global spravne
+        bruh = bool(False)
+        for i in range(n-1):
+            if dni[i] == dni[i+1]:
+                print("Toto číslo je rovnaké: "+ str(dni[i]))
+                bruh = True
+        if bruh == True:
+            spravne = spravne + 1
+
+        
 
 
-zaklad()
-porovnanie()
-print(dni)
+
+    zaklad()
+    porovnanie()
+    print(dni)
+print(str(spravne))
 
 
 
