88d5cf43d875d3dcc155df4ed7c2b9fb138b67a0
AdvencedmanSK <patrik.sramek0@gmail.com>
2020-09-11 20:04:46 +0200

    random_heslo.py finalny produkt

--- random_heslo.py
+++ random_heslo.py
@@ -2,9 +2,13 @@ import random
 import string
 
 
+def heslo(n):
+    pismena = list(string.ascii_letters+string.digits)
+    random.shuffle(pismena)
+    hotovka = str()
+    for i in range(n):
+        hotovka = hotovka + pismena[(random.randint(0, len(pismena)))]
+    print(hotovka)
 
-pismena = list(string.ascii_letters+string.digits)
-random.shuffle(pismena)
-
-print(pismena)
-
+bruh = int(input("Zadaj počet znakov: "))
+heslo(bruh)
