477424c0624265c1f10c17a2d1f57fd69887b072
AdvencedmanSK <patrik.sramek0@gmail.com>
2020-09-11 11:09:57 +0200

    Pridanie funkcie Verzia 1.1

--- narodeniny.py
+++ narodeniny.py
@@ -1,32 +1,35 @@
 import random
-global n,spravne
+import sys
+import os
+
 n = int(input("zadaj číslo :"))
 spravne = int(0)
-for i in range (100):
-    dni = []
-    def zaklad():
-        for i in range(n):
-            dni.append(random.randint(1,357))
-            dni.sort()
-        
-    def porovnanie():
-        global spravne
-        bruh = bool(False)
-        for i in range(n-1):
-            if dni[i] == dni[i+1]:
-                print("Toto číslo je rovnaké: "+ str(dni[i]))
-                bruh = True
-        if bruh == True:
-            spravne = spravne + 1
+def final():
+    for i in range (1000):
+        dni = []
+        def zaklad():
+            for i in range(n):
+                dni.append(random.randint(1,357))
+                dni.sort()
+            
+        def porovnanie():
+            global spravne
+            bruh = bool(False)
+            for i in range(n-1):
+                if dni[i] == dni[i+1]:
+                    print("Toto číslo je rovnaké: "+ str(dni[i]))
+                    bruh = True
+            if bruh == True:
+                spravne = spravne + 1
 
-        
+            
 
 
 
-    zaklad()
-    porovnanie()
-    print(dni)
-print(str(spravne))
+        zaklad()
+        porovnanie()
+final()
+print(spravne)
 
 
 
