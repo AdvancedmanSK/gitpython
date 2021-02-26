4cf608002e678889f2bcd7fad4bf8238d368e3ca
AdvencedmanSK <patrik.sramek0@gmail.com>
2021-02-26 10:27:31 +0100

    Čítanie

--- Decypher.py
+++ Decypher.py
@@ -1,4 +1,17 @@
+main_row = []
+main_int = int()
 def precitanie():
-    sifra = open("sifra.txt","r")
-    for lines in sifra.readlines():
-        print(lines)
+    global main_int, main_row
+    sifra = open("sifra.txt")
+    for lines in sifra.read().splitlines():
+        try:
+            int(lines)
+            main_int = lines
+        except:
+            main_row.append(lines)
+            print(main_row)
+
+        
+        
+precitanie()
+print(main_int)
