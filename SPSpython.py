65f6d92b8f6b26f1dec5f105fa8b549caabad109
AdvencedmanSK <patrik.sramek0@gmail.com>
2021-09-16 21:14:46 +0200

    Finalny program hotovy

--- SPSpython.py
+++ SPSpython.py
@@ -2,24 +2,30 @@ meno = []
 priezvisko = []
 row = []
 char = ('"')
-
+n = ("\n")
 
 email = open("SPS.txt", 'r')
 for line in email.readlines():
     for i in line.split(","):
         row.append(i)
+row = [x.lower() for x in row]
 
-def meno():
-    for i in range(0, len(row), 2):
-        print(row[i])
-meno()
+for i in range(0, len(row), 2):
+    meno.append(row[i])
+
+#with open("spsfinal.txt","w") as finall:
+#    for i in meno:
+#        finall.write('%s,\n' % i)   
+
 
 for i in range(1, len(row), 2):
     priezvisko.append(row[i])
 
 for idx, ele in enumerate(priezvisko):
         priezvisko[idx] = ele.replace(char, '')
-        with open("spsfinall.txt", "w") as f:
-            for line in priezvisko[idx]:
-                f.write(line)
+
+priezvisko = list(map(lambda x:x.strip(),priezvisko))
+with open("spsfinal.txt","w") as finall:
+    for i in range(0, len(priezvisko)):
+        finall.write(meno[i]+(",")+(priezvisko[i])+(",")+meno[i]+(".")+(priezvisko[i])+("@spsnmnv.sk")+(",SK")+"\n")
 
