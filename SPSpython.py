64a0c4be6c16ef9614782f8b69f35e5e184ff16b
AdvencedmanSK <patrik.sramek0@gmail.com>
2021-09-16 20:27:19 +0200

    Bruh

--- SPSpython.py
+++ SPSpython.py
@@ -1,2 +1,25 @@
 meno = []
 priezvisko = []
+row = []
+char = ('"')
+
+
+email = open("SPS.txt", 'r')
+for line in email.readlines():
+    for i in line.split(","):
+        row.append(i)
+
+def meno():
+    for i in range(0, len(row), 2):
+        print(row[i])
+meno()
+
+for i in range(1, len(row), 2):
+    priezvisko.append(row[i])
+
+for idx, ele in enumerate(priezvisko):
+        priezvisko[idx] = ele.replace(char, '')
+        with open("spsfinall.txt", "w") as f:
+            for line in priezvisko[idx]:
+                f.write(line)
+
