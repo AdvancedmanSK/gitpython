8a26f0a98e4798f4e8ad772655c16e4cda2008da
AdvencedmanSK <patrik.sramek0@gmail.com>
2022-06-19 16:26:34 +0200

    blbosti

--- SPSpython.py
+++ SPSpython.py
@@ -4,18 +4,14 @@ row = []
 char = ('"')
 n = ("\n")
 
-email = open("SPS.txt", 'r')
+email = open("SPS.txt", 'r')    #otvorí súbor SPS.txt
 for line in email.readlines():
     for i in line.split(","):
         row.append(i)
-row = [x.lower() for x in row]
+row = [x.lower() for x in row]   #precíta riadky
 
 for i in range(0, len(row), 2):
     meno.append(row[i])
-
-#with open("spsfinal.txt","w") as finall:
-#    for i in meno:
-#        finall.write('%s,\n' % i)   
 
 
 for i in range(1, len(row), 2):
@@ -25,7 +21,7 @@ for idx, ele in enumerate(priezvisko):
         priezvisko[idx] = ele.replace(char, '')
 
 priezvisko = list(map(lambda x:x.strip(),priezvisko))
-with open("spsfinal.txt","w") as finall:
+with open("spsfinal.txt","w") as finall:   #vytvorí spsfinal.txt
     for i in range(0, len(priezvisko)):
-        finall.write(meno[i]+(",")+(priezvisko[i])+(",")+meno[i]+(".")+(priezvisko[i])+("@spsnmnv.sk")+(",SK")+"\n")
+        finall.write(meno[i]+(",")+(priezvisko[i])+(",")+meno[i]+(".")+(priezvisko[i])+("@spsnmnv.sk")+(",SK")+"\n")   
 
